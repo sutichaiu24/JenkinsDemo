34f6016c7fde7d60dbd013eefebcba74510f24fd
Sudhichai_Ungsuthornrungsi <oah.s@excelbangkok.com>
2016-09-01 17:57:21 +0700

    Change Triangle formular

--- JenkinDemo/Models/Triangle.cs
+++ JenkinDemo/Models/Triangle.cs
@@ -10,7 +10,7 @@ namespace CIDemo.Models
     {
         public double CalculateSurface(double width, double height)
         {
-            var area = 0.5 * width * height;
+            var area = 1 * width * height;
             return Math.Abs(area);
 
         }
